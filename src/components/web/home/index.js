83976b82b3d072e13f09b56b5f5ffa718268f54f
asyraf795 <39008809+asyraf795@users.noreply.github.com>
2020-11-29 11:44:30 +0800

    Feature/#22 (#2)
    * first test
    
    * #22
    
    * #22
    
    Co-authored-by: Mohammad Asyraf <asyraf@senwave.com.my>
    Co-authored-by: hazmin <hazmin@mail.puncaktegap.com.my>
    Co-authored-by: hazmin <hazmin31@gmail.com>

--- src/components/web/home/index.js
+++ src/components/web/home/index.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react'
 import Carousel from './Carousel';
 import Kitchenitem from './kitechen-info';
+import Product from '../product';
+import { Grid } from '@material-ui/core';
+import Slider from "react-slick";
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products:[
+                {
+                    id: "1",
+                    name: 'Moong',
+                    price: 1.50,
+                    imgUrl: 'images/of.png'
+                },
+                {
+                    id: "2",
+                    name: 'Sunflower oil',
+                    price: 9.00,
+                    imgUrl: 'images/of1.png'
+                },
+                {
+                    id: "3",
+                    name: 'Kabuli Chana',
+                    price: 3.00,
+                    imgUrl: 'images/of2.png'
+                }
+            ],
+        };
+    }
+
     render() {
+
+        const renderProduct = this.state.products.map(product => (
+            <Product item={product}/>
+        ));
+
+        var settings = {
+            dots: false,
+            infinite: true,
+            autoplay: false,
+            speed: 2000,
+            autoplaySpeed: 2000,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        };
+
         return (
             <div>
+            
                 <Carousel />
+                <Grid container >
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12} style={{ marginTop: '9rem' }}>
+                <Slider {...settings}>
+                {renderProduct}
+               </Slider>
+                </Grid>
+                </Grid>
                 <Kitchenitem />
+
             </div>
         )
     }
